bb9c6d396f7402e5427afe8ae84671c0267fd609
Clément Pascutto <clement@tarides.com>
2019-06-27 10:54:34 +0200

    wip

--- fuzz/fuzz_types.ml
+++ fuzz/fuzz_types.ml
@@ -1,6 +1,14 @@
 open Crowbar
-module C = Crowbar
 module T = Irmin.Type
+
+(* This has to be upstreamed to Crowbar *)
+let char = map [ bytes_fixed 1 ] (fun s -> s.[0])
+
+let string = bytes
+
+let bytes = map [ string ] Bytes.of_string
+
+(* type len = [ `Int | `Int8 | `Int16 | `Int32 | `Int64 | `Fixed of int ] *)
 
 type 'a ty =
   | Unit : unit ty
@@ -11,9 +19,11 @@ type 'a ty =
   | Int64 : int64 ty
   | Float : float ty
   | String : string ty
+  (* | String_of : len -> string ty *)
   | Bytes : bytes ty
+  (* | Bytes_of : len -> bytes ty *)
   | List : 'a ty -> 'a list ty
-  (* | Array : 'a ty -> 'a array ty *)
+  | Array : 'a ty -> 'a array ty
   | Option : 'a ty -> 'a option ty
   | Pair : 'a ty * 'b ty -> ('a * 'b) ty
   | Triple : 'a ty * 'b ty * 'c ty -> ('a * 'b * 'c) ty
@@ -21,10 +31,22 @@ type 'a ty =
 
 type any = Any : 'a ty -> any
 
+(* let len_gen =
+  choose
+    [
+      const `Int;
+      const `Int8;
+      const `Int16;
+      const `Int32;
+      const `Int64;
+      map [ int ] (fun i -> `Fixed i);
+    ] *)
+
 let ty_gen =
   fix (fun ty_gen ->
       choose
-        [ const (Any Unit);
+        [
+          const (Any Unit);
           const (Any Bool);
           const (Any Char);
           const (Any Int);
@@ -32,13 +54,18 @@ let ty_gen =
           const (Any Int64);
           const (Any Float);
           const (Any String);
+          (* map [ len_gen ] (fun len -> Any (String_of len)); *)
           const (Any Bytes);
+          (* map [ len_gen ] (fun len -> Any (Bytes_of len)); *)
           map [ ty_gen ] (fun (Any t) -> Any (List t));
+          map [ ty_gen ] (fun (Any t) -> Any (Array t));
           map [ ty_gen ] (fun (Any t) -> Any (Option t));
           map [ ty_gen; ty_gen ] (fun (Any t1) (Any t2) -> Any (Pair (t1, t2)));
-          map [ ty_gen; ty_gen; ty_gen ] (fun (Any t1) (Any t2) (Any t3) -> Any (Triple (t1, t2, t3)));
-          map [ ty_gen; ty_gen ] (fun (Any t1) (Any t2) -> Any (Result (t1, t2)));
-        ] )
+          map [ ty_gen; ty_gen; ty_gen ] (fun (Any t1) (Any t2) (Any t3) ->
+              Any (Triple (t1, t2, t3)));
+          map [ ty_gen; ty_gen ] (fun (Any t1) (Any t2) ->
+              Any (Result (t1, t2)));
+        ])
 
 let rec ty_to_irmin : type a. a ty -> a T.ty = function
   | Unit -> T.unit
@@ -49,17 +76,20 @@ let rec ty_to_irmin : type a. a ty -> a T.ty = function
   | Int64 -> T.int64
   | Float -> T.float
   | String -> T.string
+  (* | String_of i -> T.string_of i *)
   | Bytes -> T.bytes
+  (* | Bytes_of i -> T.bytes_of i *)
   | List ty -> T.list (ty_to_irmin ty)
+  | Array ty -> T.array (ty_to_irmin ty)
   | Option ty -> T.option (ty_to_irmin ty)
   | Pair (ty1, ty2) -> T.pair (ty_to_irmin ty1) (ty_to_irmin ty2)
-  | Triple (ty1, ty2, ty3) -> T.triple (ty_to_irmin ty1) (ty_to_irmin ty2) (ty_to_irmin ty3)
+  | Triple (ty1, ty2, ty3) ->
+      T.triple (ty_to_irmin ty1) (ty_to_irmin ty2) (ty_to_irmin ty3)
   | Result (ty1, ty2) -> T.result (ty_to_irmin ty1) (ty_to_irmin ty2)
 
-let ty_to_irmin_gen t = C.const (ty_to_irmin t)
+let ty_to_irmin_gen t = const (ty_to_irmin t)
 
-let triple a b c =
-  map [a; b; c] (fun a b c -> (a, b, c))
+let triple a b c = map [ a; b; c ] (fun a b c -> (a, b, c))
 
 let rec ty_to_value_gen : type a. a ty -> a gen = function
   | Unit -> const ()
@@ -70,17 +100,21 @@ let rec ty_to_value_gen : type a. a ty -> a gen = function
   | Int64 -> int64
   | Float -> float
   | String -> string
+  (* | String_of _ -> string *)
   | Bytes -> bytes
+  (* | Bytes_of _ -> bytes *)
   | List ty -> list (ty_to_value_gen ty)
-  | Option ty  -> option (ty_to_value_gen ty)
+  | Array ty -> map [ list (ty_to_value_gen ty) ] Array.of_list
+  | Option ty -> option (ty_to_value_gen ty)
   | Pair (ty1, ty2) -> pair (ty_to_value_gen ty1) (ty_to_value_gen ty2)
-  | Triple (ty1, ty2, ty3) -> triple (ty_to_value_gen ty1) (ty_to_value_gen ty2)(ty_to_value_gen ty3)
+  | Triple (ty1, ty2, ty3) ->
+      triple (ty_to_value_gen ty1) (ty_to_value_gen ty2) (ty_to_value_gen ty3)
   | Result (ty1, ty2) -> result (ty_to_value_gen ty1) (ty_to_value_gen ty2)
 
 type pair = Pair : 'a T.t * 'a -> pair
 
 let pair_gen : pair gen =
-  C.dynamic_bind ty_gen @@ fun (Any t) ->
+  dynamic_bind ty_gen @@ fun (Any t) ->
   let itype_gen = ty_to_irmin_gen t in
   let val_gen = ty_to_value_gen t in
   map [ itype_gen; val_gen ] (fun t v -> Pair (t, v))
@@ -89,6 +123,6 @@ let check_pair (Pair (t, v)) =
   let encoded = T.to_bin_string t v in
   match T.of_bin_string t encoded with
   | Error _ -> fail "incorrect deserialization"
-  | Ok v' -> C.check_eq v v'
+  | Ok v' -> check_eq v v'
 
 let () = add_test ~name:"Irmin.Type" [ pair_gen ] check_pair
